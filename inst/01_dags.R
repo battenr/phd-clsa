eaf0140089a7c5c5765c354819b6504c46a8042b
battenr <rmb788@mun.ca>
2023-06-21 09:13:25 -0300

    initial commit of code for DAGs

--- inst/01_dags.R
+++ inst/01_dags.R
@@ -26,3 +26,30 @@ lapply(list.files("R/"), function(x) source(paste0("R/", x)))
 # potential bias while also avoiding additional biases such as adjusting for a collider and causing
 # an open path
 
+# Covariates associated with BZD use: 
+# marital status, age, sex, income, province, 
+
+
+# Anxiety  ----
+
+# Depression  ----
+
+# Diabetes Mellitus  ----
+
+# High Blood Pressure  ----
+
+# Myocardial Infarction  ----
+
+# Stroke  ----
+
+# Cancer  ----
+
+# Heart Disease  ----
+
+# COPD  ----
+
+# Dementia  ----
+
+# Infections  ----
+
+# Pneumonia  ----

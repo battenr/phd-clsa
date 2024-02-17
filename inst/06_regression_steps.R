7cc8108d8fedbed76cc0a736911484b544532b14
battenr <ryan.batten@mun.ca>
2024-02-16 20:04:52 -0400

    beginning to work through example for regression analyses

--- inst/06_regression_steps.R
+++ inst/06_regression_steps.R
@@ -29,13 +29,21 @@ load("data/analytic_dataset.Rdata")
 # 5. Check if confounder
 # [(theta | c) - (theta | no c)] / (theta | c)
 
-# 6. Check plausible interaction terms: 
+# 6. Check plausible interaction terms: (four)
+
+age*sex
+household_income*education
+age*urban_rural
+urban_rural*province
+
 # 
 
 # 7. Check assumptions
 
 # - Check that errors are independent (similar to MSc)
-# - Check leverage and Cook's distance
+# - Check leverage and Cook's distance for outliers
+# - Collinearity (calculate VIF) [leaving this out for now bc it's a survey GLM
+# which is different from a GLM, it's doesn't use MLE]
 
 # Code to use ####
 
@@ -45,7 +53,62 @@ df %>% colnames()
 
 #... Univariate Analysis ----
 
-# 
+# Using Depression as the Example to Work Through ----
+
+# Setup ----
+
+#... Libraries ----
+
+library(tidyverse)
+library(survey)
+library(officer)
+library(flextable)
+
+
+#... Functions ----
+
+# Load all functions
+
+lapply(list.files("R/"), function(x) source(paste0("R/", x)))
+
+#... Data ----
+
+load("data/analytic_dataset.Rdata")
+
+# Setting analytic weights for survey 
+
+outcome = "depression"
+
+covars <- c("bzd",
+            "age", 
+            "sex",
+            "province",
+            "marital_status",
+            "smoke",
+            "education",
+            "household_income",
+            "urban_rural", 
+            "wghts_analytic", 
+            "geostrata"
+)
+
+design.analytic <- svydesign(data = df %>% 
+                               select(
+                                 covars, 
+                                 outcome) %>%
+                               drop_na(),
+                             weights= ~wghts_analytic, 
+                             strata = ~geostrata,
+                             #fpc = ~strata_total,
+                             ids = ~1,
+                             nest = TRUE)
+#... Univariate Analysis 
+
+covars %>% 
+  purrr::map(
+    # map function for univariate analysis across the covars objects
+  )
+
 
 
 
